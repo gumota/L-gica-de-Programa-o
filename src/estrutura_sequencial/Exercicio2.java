840501e1f9f2608dffd39f3b2fed6e2641f75659
Gustavo Mota <gustavo.omota@hotmail.com>
2020-03-12 10:47:09 -0300

    Novos algoritmos

---
+++ src/estrutura_sequencial/Exercicio2.java
@@ -0,0 +1,20 @@
+package estrutura_sequencial;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+	private static Scanner in;
+
+	public static void main(String[] args) {
+
+		in = new Scanner(System.in);
+		double salario;
+
+		System.out.println("Digite seu salário:");
+		salario = in.nextDouble();
+		System.out.println("Seu novo salário é R$ " + salario * 1.15);
+
+	}
+
+}
