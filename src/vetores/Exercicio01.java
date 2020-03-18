2cba358ccb44aed40faeed9b904812f1475a789a
Gustavo Mota <gustavo.omota@hotmail.com>
2020-03-18 10:20:19 -0300

    vetor01

--- src/vetores/Exercicio01.java
+++ src/vetores/Exercicio01.java
@@ -1,5 +1,37 @@
 package vetores;
 
+import java.util.Scanner;
+
 public class Exercicio01 {
+
+	private static Scanner in;
+
+	public static void main(String[] args) {
+
+		in = new Scanner(System.in);
+		int[] vet = new int[5];
+		double media = 0;
+		int cont = 0;
+		int impares = 0;
+
+		System.out.println("Digite 50 valores e obtenha dados sobre eles");
+
+		for (int i = 0; i < 5; i++) {
+			vet[i] = in.nextInt();
+
+			if (vet[i] >= 10 && vet[i] <= 200) {
+				media += vet[i];
+				cont++;
+
+			}
+			if (vet[i] % 2 != 0) {
+				impares += vet[i];
+			}
+
+		}
+		System.out.println(media = media / cont);
+		System.out.println(impares);
+
+	}
 
 }
