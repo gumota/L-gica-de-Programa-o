d4b7efc4e61e7ae0c3008b3881537215f4d0b5e8
Gustavo Mota <gustavo.omota@hotmail.com>
2020-03-17 22:42:00 -0300

    Novos

---
+++ src/estrutura_sequencial/Exercicio4.java
@@ -0,0 +1,23 @@
+package estrutura_sequencial;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+
+	private static Scanner in;
+
+	public static void main(String[] args) {
+
+		in = new Scanner(System.in);
+		int celsius;
+
+		System.out.println("Digite a temperatura em graus Celsius e obtenha em fahrenheit");
+		celsius = in.nextInt();
+
+		double fahrenheit = ((9 * celsius) + 160) / 5;
+
+		System.out.println(fahrenheit);
+
+	}
+
+}
---
+++ src/estrutura_sequencial/Exercicio5.java
@@ -0,0 +1,15 @@
+package estrutura_sequencial;
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+
+	private static Scanner in;
+
+	public static void main(String[] args) {
+
+		in = new Scanner(System.in);
+
+	}
+
+}
